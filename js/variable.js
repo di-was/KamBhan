93274c16086604bf373e1d09274968bc0f4be2de
di-was <diwasdahal109@gmail.com>
2022-10-15 09:20:40 +0545

    Variables declarations

---
+++ js/variable.js
@@ -0,0 +1,15 @@
+// Contains variable declarations for the app
+
+// Buttons
+let newTask = document.querySelector('.')
+let themeToggle = document.querySelector('.')
+let hamburgerIcon = document.querySelector('.')
+let sideBarVisibility = document.querySelector('.')
+let newBoard = document.querySelector('.')
+let boards = document.querySelectorAll('.') // Contains array of buttons
+
+const variables  = {
+    newTask, themeToggle, hamburgerIcon, sideBarVisibility, newBoard, boards
+}
+
+export default variables
